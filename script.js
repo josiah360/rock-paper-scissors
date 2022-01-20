485a5454f03ba31ab6745cdda6bb4a80618bff43
Josiah <josiaholajide@gmail.com>
2022-01-20 07:24:52 +0100

    Create a function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let options = ['rock', 'paper', 'scissors'];
+
+function computerSelection(){
+    let randomSelect = Math.floor(Math.random() * options.length);
+    let option = options[randomSelect];
+    return option;
+}
+
+function playerSelection() {
+    
+}
