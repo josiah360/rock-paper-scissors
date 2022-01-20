5311b0da210fab271657abd0ba076283cf3d2379
Josiah <josiaholajide@gmail.com>
2022-01-20 08:18:53 +0100

    Create a function called gameRound() that plays a single round of the game and returns the winner. It take two parameters: player choice and computer choice

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-let options = ['rock', 'paper', 'scissors'];
+let options = ['scissors', 'rock', 'paper'];
+let playerScore = '';
 
-function computerSelection(){
+function computerPlay(){
     let randomSelect = Math.floor(Math.random() * options.length);
     let option = options[randomSelect];
     return option;
 }
 
-function playerSelection() {
-    
+function gameRound(play, computer) {
+    let player = play.toLowerCase();
+    let comp = computer.toLowerCase();
+    let message;
+
+    switch (true) {
+        case (player === 'rock' && comp === 'scissors'):
+        case (player === 'scissors' && comp === 'paper'):
+        case (player === 'paper' && comp === 'rock'):
+            message = `You Won! ${player} beat ${comp}`;
+            break;
+           
+        case (player === 'scissors' && comp === 'rock'):
+        case (player === 'paper' && comp === 'scissors'):
+        case (player === 'rock' && comp === 'paper'):
+            message = `You Lost! ${comp} beats ${player}`;
+            break;
+
+        default:
+            message = 'It\'s a Draw!';
+    }
+
+    return message;
 }
+
+let playerSelection = 'scissors';
+let computerSelection = computerPlay();
+
+console.log(gameRound(playerSelection, computerSelection));
